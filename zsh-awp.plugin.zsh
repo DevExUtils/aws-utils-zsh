f0956a4b6e77b84b95668d3ac7dabec18c207a53
Henrik Stanley Mortensen <31671391+HenrikStanley@users.noreply.github.com>
2022-04-19 20:26:38 +0200

    Update zsh-awp.plugin.zsh

--- zsh-awp.plugin.zsh
+++ zsh-awp.plugin.zsh
@@ -3,20 +3,25 @@
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
+# if [ -v "$FZF_DEFAULT_OPTS" ] 
+# then
+# 	fzf_awp_opts=${FZF_DEFAULT_OPTS}
+# else
+# 	fzf_awp_opts="'--height 60%' '--layout=reverse' '--border'"
+# fi
 
-if [ -v "$FZF_DEFAULT_OPTS" ] 
-then
-	FZF_AWP_OPTS=$FZF_DEFAULT_OPTS
-else
-	fzf_awp_opts="--height 60% --layout=reverse --border"
-fi
+fzf_awp_opts=(
+    --ansi
+    --height 60%
+    --layout=reverse
+    --border
+)
 
 awp() {
   local aws_profile
   if [[ "$#" -eq 0 ]]; then
-    aws_profile=$(aws configure list-profiles | fzf $fzf_awp_opts
-  elif [[ "$#" -eq 1 ]]; then
-    aws_profile=$(aws configure list-profiles | fzf -q ${1} --select-1 --exit-0 $fzf_awp_opts)
+    aws_profile=$(aws configure list-profiles | fzf --height 60% --layout=reverse --border)
+    aws_profile=$(aws configure list-profiles | fzf -q ${1} --select-1 --exit-0 --height 60% --layout=reverse --border)
   fi
   export AWS_PROFILE="${aws_profile}"
 }
@@ -27,12 +32,12 @@ awr() {
     aws_region=$(aws ec2 describe-regions \
                   --all-regions \
                   --query "Regions[].{Name:RegionName}" \
-                  --output text | fzf $fzf_awp_opts)
+                  --output text | fzf --height 60% --layout=reverse --border)
   elif [[ "$#" -eq 1 ]]; then
     aws_region=$(aws ec2 describe-regions \
                   --all-regions \
                   --query "Regions[].{Name:RegionName}" \
-                  --output text | fzf -q ${1} --select-1 --exit-0 $fzf_awp_opts)
+                  --output text | fzf -q ${1} --select-1 --exit-0 --height 60% --layout=reverse --border)
   fi
   export AWS_REGION="${aws_region}"
 }
